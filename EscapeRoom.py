75b021e5f78d51375ddea25c19411077eece6cc4
Ralfs Īzars <izarsralfs4@gmail.com>
2025-12-09 12:28:10 +0200

    Test

--- EscapeRoom.py
+++ EscapeRoom.py
@@ -4,7 +4,7 @@ import vizshape
 import vizact
 import random
 
-# ---------- Setup ----------
+# ---------- setup ----------
 viz.setOption('viz.fullscreen', '1')
 viz.go()
 
@@ -68,41 +68,35 @@ def createInventoryUI():
         x_pos = 0.25 + (i * 0.13)
         slotBG = viz.addText('[     ]', parent=viz.SCREEN)
         slotBG.setPosition(x_pos, 0.1)
-        slotBG.fontSize(42)
+        slotBG.fontSize = 42
         slotBG.alignment(viz.ALIGN_CENTER_CENTER)
         slotBG.color(1,1,0 if i == selectedSlot else 0.7)
-        slotText = viz.addText(str(i + 1), parent=viz.SCREEN)
-        slotText.setPosition(x_pos, 0.05)
-        slotText.fontSize(20)
+        slotText = viz.addText(str(i+1), parent=viz.SCREEN)
+        slotText.setPosition(x_pos,0.05)
+        slotText.fontSize = 20
         slotText.alignment(viz.ALIGN_CENTER_CENTER)
         codeText = viz.addText('', parent=viz.SCREEN)
-        codeText.setPosition(x_pos, 0.105)
-        codeText.fontSize(18)
+        codeText.setPosition(x_pos,0.105)
+        codeText.fontSize = 18
         codeText.alignment(viz.ALIGN_CENTER_CENTER)
         codeText.color(1,1,0)
-        codeText.visible(False)
-        inventoryUI.append({
-            'background': slotBG,
-            'slotNumber': slotText,
-            'codeText': codeText
-        })
+        codeText.visible = False
+        inventoryUI.append({'background': slotBG,'slotNumber':slotText,'codeText':codeText})
 
 def updateInventoryUI():
     for i in range(MAX_INVENTORY_SLOTS):
-        inventoryUI[i]['background'].color(
-            1,1,0 if i == selectedSlot else 0.7
-        )
-        if inventory[i] and inventory[i]['type'] == 'code':
+        inventoryUI[i]['background'].color(1,1,0 if i==selectedSlot else 0.7)
+        if inventory[i] and inventory[i]['type']=='code':
             codePart = inventory[i]['name'].split(': ')[1]
             inventoryUI[i]['codeText'].message(codePart)
-            inventoryUI[i]['codeText'].visible(True)
+            inventoryUI[i]['codeText'].visible=True
         else:
-            inventoryUI[i]['codeText'].visible(False)
+            inventoryUI[i]['codeText'].visible=False
 
-def addToInventory(itemName, itemType='generic'):
+def addToInventory(itemName,itemType='generic'):
     for i in range(MAX_INVENTORY_SLOTS):
         if inventory[i] is None:
-            inventory[i] = {'name': itemName, 'type': itemType}
+            inventory[i] = {'name':itemName,'type':itemType}
             updateInventoryUI()
             return True
     return False
@@ -116,7 +110,7 @@ def removeFromInventory(slotIndex):
 
 def useItem(slotIndex):
     if inventory[slotIndex]:
-        if inventory[slotIndex]['type'] == 'code':
+        if inventory[slotIndex]['type']=='code':
             print("Code cannot be used with E")
             return None
         removed = inventory[slotIndex]
@@ -145,10 +139,10 @@ crosshair.fontSize = 24
 crosshair.alignment(viz.ALIGN_CENTER_CENTER)
 
 # --------- STICKY NOTE ----------
-noteCode = str(random.randint(1000, 9999))
+noteCode = str(random.randint(100000, 999999))  # longer code
 noteObject = None
 notePickedUp = False
-NOTE_POSITION = [-7.2, 5.7, 11.2]
+NOTE_POSITION = [-7.2,5.7,11.2]
 NOTE_PICKUP_RADIUS = 2.0
 
 def createNote():
@@ -156,23 +150,24 @@ def createNote():
     noteObject = viz.addChild('StickyNote.fbx')
     noteObject.setPosition(NOTE_POSITION)
     noteObject.setScale([0.5,0.5,0.5])
-    noteObject.setEuler([0, 180, 0])
+    noteObject.setEuler([0,180,0])
     noteObject.disable(viz.DYNAMICS)
     print("Sticky note created at:", noteObject.getPosition())
 
 def checkNotePickup():
     if not noteObject or notePickedUp:
+        crosshair.color(1,1,1)
         return
     p = viz.MainView.getPosition()
     n = noteObject.getPosition()
-    d = ((p[0]-n[0])**2 + (p[1]-n[1])**2 + (p[2]-n[2])**2) ** 0.5
-    if d < NOTE_PICKUP_RADIUS:
+    d = ((p[0]-n[0])**2 + (p[1]-n[1])**2 + (p[2]-n[2])**2)**0.5
+    if d<NOTE_PICKUP_RADIUS:
         crosshair.color(1,1,0)
     else:
         crosshair.color(1,1,1)
 
 # --------- SAFE SYSTEM ----------
-SAFE_POSITION = [-5, 5.7, 9]
+SAFE_POSITION = [-5,5.7,9]
 SAFE_RADIUS = 2.0
 safeLocked = True
 keyTaken = False
@@ -181,66 +176,62 @@ KEY_ITEM_NAME = "Door Key"
 def checkSafeProximity():
     if safeLocked and not keyTaken:
         p = viz.MainView.getPosition()
-        dist = ((p[0]-SAFE_POSITION[0])**2 + (p[1]-SAFE_POSITION[1])**2 + (p[2]-SAFE_POSITION[2])**2) ** 0.5
+        dist = ((p[0]-SAFE_POSITION[0])**2 + (p[1]-SAFE_POSITION[1])**2 + (p[2]-SAFE_POSITION[2])**2)**0.5
         if dist < SAFE_RADIUS:
-            crosshair.color(0,1,1)  # cyan
+            crosshair.color(0,1,1)
         else:
             crosshair.color(1,1,1)
 
-# --------- DOOR SYSTEM ----------
-DOOR_POSITION = [-2, 0, 14]
-DOOR_RADIUS = 2.5
-doorLocked = True
-
-def checkDoorProximity():
-    p = viz.MainView.getPosition()
-    dist = ((p[0]-DOOR_POSITION[0])**2 + (p[1]-DOOR_POSITION[1])**2 + (p[2]-DOOR_POSITION[2])**2) ** 0.5
-    if dist < DOOR_RADIUS:
-        crosshair.color(0,1,0)
-    else:
-        crosshair.color(1,1,1)
-
-# --------- F key: note pickup & safe unlock ----------
 def onKeyF():
     global notePickedUp, noteObject, safeLocked, keyTaken
     p = viz.MainView.getPosition()
-
-    # Try pickup note
+    # Pickup sticky note
     if noteObject and not notePickedUp:
         n = noteObject.getPosition()
-        d_note = ((p[0]-n[0])**2 + (p[1]-n[1])**2 + (p[2]-n[2])**2) ** 0.5
+        d_note = ((p[0]-n[0])**2 + (p[1]-n[1])**2 + (p[2]-n[2])**2)**0.5
         if d_note < NOTE_PICKUP_RADIUS:
-            addToInventory(f"Safe Code: {noteCode}", "code")
+            addToInventory(f"Safe Code: {noteCode}","code")
             noteObject.remove()
             noteObject = None
             notePickedUp = True
-            print("Sticky note picked up! Code:", noteCode)
+            print("Sticky note picked up! Code:",noteCode)
             return
-
-    # Try unlock safe
+    # Open safe
     if safeLocked and not keyTaken:
-        dist_safe = ((p[0]-SAFE_POSITION[0])**2 + (p[1]-SAFE_POSITION[1])**2 + (p[2]-SAFE_POSITION[2])**2) ** 0.5
+        dist_safe = ((p[0]-SAFE_POSITION[0])**2 + (p[1]-SAFE_POSITION[1])**2 + (p[2]-SAFE_POSITION[2])**2)**0.5
         if dist_safe < SAFE_RADIUS:
-            hasCode = any(item and item['type'] == 'code' for item in inventory)
+            hasCode = any(item and item['type']=='code' for item in inventory)
             if hasCode:
                 print("Correct code! Safe unlocked.")
                 safeLocked = False
-                addToInventory(KEY_ITEM_NAME, "key")
+                addToInventory(KEY_ITEM_NAME,'key')
                 keyTaken = True
             else:
                 print("The safe is locked. You need the code.")
 
 vizact.onkeydown('f', onKeyF)
 
-# --------- T key: open door ----------
+# --------- DOOR SYSTEM ----------
+DOOR_POSITION = [-2,0,14]
+DOOR_RADIUS = 2.5
+doorLocked = True
+
+def checkDoorProximity():
+    p = viz.MainView.getPosition()
+    dist = ((p[0]-DOOR_POSITION[0])**2 + (p[1]-DOOR_POSITION[1])**2 + (p[2]-DOOR_POSITION[2])**2)**0.5
+    if dist<DOOR_RADIUS:
+        crosshair.color(0,1,0)
+    else:
+        crosshair.color(1,1,1)
+
 def tryOpenDoor():
     global doorLocked
     p = viz.MainView.getPosition()
-    dist = ((p[0]-DOOR_POSITION[0])**2 + (p[1]-DOOR_POSITION[1])**2 + (p[2]-DOOR_POSITION[2])**2) ** 0.5
-    if dist > DOOR_RADIUS:
+    dist = ((p[0]-DOOR_POSITION[0])**2 + (p[1]-DOOR_POSITION[1])**2 + (p[2]-DOOR_POSITION[2])**2)**0.5
+    if dist>DOOR_RADIUS:
         return
     if doorLocked:
-        hasKey = any(item and item['name'] == KEY_ITEM_NAME for item in inventory)
+        hasKey = any(item and item['name']==KEY_ITEM_NAME for item in inventory)
         if hasKey:
             print("Door unlocked! You escaped!")
             doorLocked = False
@@ -250,7 +241,19 @@ def tryOpenDoor():
 
 vizact.onkeydown('t', tryOpenDoor)
 
-# --------- INIT ----------
+# ---------- MODEL ----------
+room = viz.addChild('TestRoom.osgb')
+room.setPosition(-10,0,-10)
+room.setScale([0.05,0.05,0.05])
+room.enable(viz.LIGHTING)
+
+movingBooks = room.getTransform('movingBooks')
+def kustinatGramatu():
+    action = vizact.moveTo([339.81381,120,92.62527], time=2.0)
+    movingBooks.addAction(action)
+vizact.onkeydown('g', kustinatGramatu)
+
+# ---------- INIT ----------
 def initializeInventoryUI():
     createInventoryUI()
     createNote()
